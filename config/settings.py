40d45523169a4f924e1253d58553d2aca730f92b
usernamehs <hsforwork123@gmail.com>
2025-01-11 19:35:48 +0900

    Feat: 2주차 세션 자료

--- config/settings.py
+++ config/settings.py
@@ -62,12 +62,15 @@ DJANGO_APPS = [
 
 PROJECT_APPS = [
     "posts",
+    "accounts",
 ]
 
 THIRD_PARTY_APPS = []
 
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
+
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+class  Post(models.Model):
+
+    CHOICES = (
+        ('STORED', '보관'),
+        ('PUBLISHED', '발행')
+    )
+
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    status = models.CharField(max_length=15, choices=CHOICES, default='STORED')
+    created = models.DateTimeField(auto_now_add=True) # 객체를 생성할 때 날짜와 시간 저장
+    updated = models.DateTimeField(auto_now=True) # 객체를 저장할 때 날짜와 시간 갱신
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
