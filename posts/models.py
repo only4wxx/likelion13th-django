34cf5a660df886fe9865b7e888afaa739dd2af6f
usernamehs <hsforwork123@gmail.com>
2025-01-12 03:01:35 +0900

    Feat: BaseModel 추가

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,15 @@ from django.db import models
 from accounts.models import User
 
 # Create your models here.
-class  Post(models.Model):
+class BaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True) # 객체를 생성할 때 날짜와 시간 저장
+    updated = models.DateTimeField(auto_now=True) # 객체를 저장할 때 날짜와 시간 갱신
+
+    class Meta:
+        abstract = True
+
+
+class  Post(BaseModel):
 
     CHOICES = (
         ('STORED', '보관'),
@@ -13,10 +21,7 @@ class  Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
     status = models.CharField(max_length=15, choices=CHOICES, default='STORED')
-    created = models.DateTimeField(auto_now_add=True) # 객체를 생성할 때 날짜와 시간 저장
-    updated = models.DateTimeField(auto_now=True) # 객체를 저장할 때 날짜와 시간 갱신
-
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
 
     def __str__(self):
         return self.title
